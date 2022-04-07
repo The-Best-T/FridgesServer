faca346873e65173fa684f44fc8f69fda7571d7c
Fallen-Angel-FF <zhamichenok@gmail.com>
2022-04-07 19:14:24 +0300

    add doc for fridgeProducts controller

--- Server/Controllers/FridgeProductsController.cs
+++ Server/Controllers/FridgeProductsController.cs
@@ -43,7 +43,19 @@ namespace Server.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Get one product from fridge by fridge id and product id
+        /// </summary>
+        /// <param name="fridgeId"></param>
+        /// <param name="productId"></param>
+        /// <returns>One product from the fridge</returns>
+        /// <response code="200">Returns one product from the fridge</response>
+        /// <response code="404">Fridge or product in this fridge with this id not found</response>
+        /// <response code="500">Server error</response>
         [HttpGet("{productId}", Name = "GetProductForFridge")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
         [ServiceFilter(typeof(ValidateFridgeProductExistsAttribute))]
         public IActionResult GetProductForFridge(Guid fridgeId, Guid productId)
         {
@@ -53,8 +65,20 @@ namespace Server.Controllers
             return Ok(fridgeProductDto);
         }
 
+        /// <summary>
+        /// Get all product from fridge 
+        /// </summary>
+        /// <param name="fridgeId"></param>
+        /// <param name="parameters"></param>
+        /// <returns>List of products from the fridge </returns>
+        /// <response code="200">Returns list of products from the fridge</response>
+        /// <response code="404">Fridge with this id not found</response>
+        /// <response code="500">Server error</response>
         [HttpGet]
         [HttpHead]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
         [ServiceFilter(typeof(ValidateFridgeExistsAttribute))]
         public async Task<IActionResult> GetProductsForFridge(Guid fridgeId,
             [FromQuery] FridgeProductParameters parameters)
@@ -68,7 +92,23 @@ namespace Server.Controllers
             return Ok(FridgeProductsDto);
         }
 
+        /// <summary>
+        /// Add new product in fridge
+        /// </summary>
+        /// <param name="fridgeId"></param>
+        /// <param name="fridgeProduct"></param>
+        /// <returns>The newly product from the fridge</returns>
+        /// <response code="201">Returns the newly product from the fridge</response>
+        /// <response code="400">FridgeProductForCreationDto is null</response>
+        /// <response code="404">Fridge or product with this id not found</response>
+        /// <response code="422">FridgeProductForCreationDto is invalid</response>
+        /// <response code="500">Server error</response>
         [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(422)]
+        [ProducesResponseType(500)]
         [Authorize(Roles = "Administrator")]
         [ServiceFilter(typeof(ValidationFilterAttribute))]
         [ServiceFilter(typeof(ValidateFridgeExistsAttribute))]
@@ -101,7 +141,19 @@ namespace Server.Controllers
                  fridgeProductToReturn);
         }
 
+        /// <summary>
+        /// Delete product from fridge
+        /// </summary>
+        /// <param name="fridgeId"></param>
+        /// <param name="productId"></param>
+        /// <returns></returns>
+        /// <response code="204">Product deleted from the fridge</response>
+        /// <response code="404">Fridge or product in this fridge with this id not found</response>
+        /// <response code="500">Server error</response>
         [HttpDelete("{productId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
         [Authorize(Roles = "Administrator")]
         [ServiceFilter(typeof(ValidateFridgeProductExistsAttribute))]
         public async Task<IActionResult> DeleteProductFromFridge(Guid fridgeId, Guid productId)
@@ -114,7 +166,25 @@ namespace Server.Controllers
             return NoContent();
         }
 
+
+        /// <summary>
+        /// Update product in fridge
+        /// </summary>
+        /// <param name="fridgeId"></param>
+        /// <param name="productId"></param>
+        /// <param name="fridgeProduct"></param>
+        /// <returns></returns>
+        /// <response code="204">Product deleted from the fridge</response>
+        /// <response code="400">FridgeProductForCreationDto is null</response>
+        /// <response code="404">Fridge or product with this id not found</response>
+        /// <response code="422">FridgeProductForCreationDto is invalid</response>
+        /// <response code="500">Server error</response>
         [HttpPut("{productId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(422)]
+        [ProducesResponseType(500)]
         [Authorize(Roles = "Administrator")]
         [ServiceFilter(typeof(ValidationFilterAttribute))]
         [ServiceFilter(typeof(ValidateFridgeProductExistsAttribute))]
