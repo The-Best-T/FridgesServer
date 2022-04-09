925f601b768510731ac68aed33bbb451ff05dd44
Fallen-Angel-FF <zhamichenok@gmail.com>
2022-04-10 00:31:07 +0300

    fix mistake

--- Entities/DTO/User/UserForAuthenticationDto.cs
+++ Entities/DTO/User/UserForAuthenticationDto.cs
@@ -7,7 +7,7 @@ namespace Entities.Dto.User
         [Required(ErrorMessage = "User name is required")]
         public string UserName { get; set; }
 
-        [Required(ErrorMessage = "Password name is required")]
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
     }
 }
